b0155e7489c6ef29a479402945199bb7125d8466
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-04-17 02:33:23 -0700

    feat: add wsl to sync.sh

--- sync.sh
+++ sync.sh
@@ -4,21 +4,31 @@
 # License: GNU GPLv3
 
 macos_code_dir="${HOME}/Code"
+wsl_code_dir="${HOME}/code"
 
 # Navigate to current directory
 cd "$(dirname "${0}")" || exit
 
+# macOS installation
 if [ "$(uname -s)" = 'Darwin' ]; then
   mkdir -p "${macos_code_dir}" && \
     cp editor/.editorconfig "${macos_code_dir}/.editorconfig"
 
   mkdir -p "${HOME}/.config/kitty" && \
-    cp -r kitty/ "${HOME}/.config/kitty/"
-
-  mkdir -p "${HOME}/.config/nvim/" && \
-    cp -r nvim/ "${HOME}/.config/nvim/"
-
-  mkdir -p "${HOME}/.config/zsh/" && \
-    cp -r zsh/ "${HOME}/.config/zsh/" && \
-    mv "${HOME}/.config/zsh/.zshenv" "${HOME}/.zshenv"
+    cp -r kitty/ "${HOME}/.config/"
 fi
+
+# WSL installation
+if grep -q microsoft /proc/version; then
+  mkdir -p "${wsl_code_dir}" && \
+    cp editor/.editorconfig "${wsl_code_dir}/.editorconfig" 
+fi
+
+# All installations
+mkdir -p "${HOME}/.config/nvim/" && \
+  cp -r nvim/ "${HOME}/.config/"
+
+mkdir -p "${HOME}/.config/zsh/" && \
+  cp -r zsh/ "${HOME}/.config/" && \
+  mv "${HOME}/.config/zsh/.zshenv" "${HOME}/.zshenv"
+
