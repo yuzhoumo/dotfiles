70e3231fdca20e33bf9246ba6787592f210265bb
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-07-10 01:33:47 -0700

    refactor: clean up configs and add git config sync

--- sync.sh
+++ sync.sh
@@ -3,39 +3,49 @@
 # Author: Yuzhou "Joe" Mo (@yuzhoumo)
 # License: GNU GPLv3
 
-macos_code_dir="${HOME}/Code"
-wsl_code_dir="${HOME}/github"
-
 # Navigate to current directory
 cd "$(dirname "${0}")" || exit
 
 if [ "$(uname -s)" = 'Darwin' ]; then # macOS installation
 
-  mkdir -p "${macos_code_dir}" && \
-    cp editor/.editorconfig "${macos_code_dir}/.editorconfig"
-  
+  # directory for git repos
+  CODE_DIR="${HOME}/Code"
+
+  # kitty config
   mkdir -p "${HOME}/.config/kitty" && \
     cp -r kitty/. "${HOME}/.config/kitty/."
 
 elif grep -q microsoft /proc/version; then # WSL installation
-  
-  mkdir -p "${wsl_code_dir}" && \
-    cp editor/.editorconfig "${wsl_code_dir}/.editorconfig"
+
+  # directory for git repos
+  CODE_DIR="${HOME}/code"
 
 fi
 
-# All installations
-# NOTE: period after trailing slash is significant to
-# ensure consistent behavior between both BSD and GNU
-# implementations of "cp" (eg. macOS vs Linux respectively)
-# https://ackshaey.medium.com/835c0b608339
-mkdir -p "${HOME}/.config/nvim/" && \
-  cp -r nvim/. "${HOME}/.config/nvim/."
+# note: period after trailing slash is significant to ensure consistent
+# behavior between both BSD and GNU implementations of "cp" (eg. macOS vs
+# Linux respectively); https://ackshaey.medium.com/835c0b608339
 
+# zsh config
 mkdir -p "${HOME}/.config/zsh/" && \
   cp -r zsh/. "${HOME}/.config/zsh/." && \
   mv "${HOME}/.config/zsh/.zshenv" "${HOME}/.zshenv"
 
+# editor config
+mkdir -p "${CODE_DIR}" && \
+  cp editor/.editorconfig "${CODE_DIR}/.editorconfig"
+
+# git config
+mkdir -p "${HOME}/.config/git/" && \
+  cp -r git/. "${HOME}/.config/git/." && \
+  mv "${HOME}/.config/git/.gitignore" "${HOME}/.gitignore" && \
+  mv "${HOME}/.config/git/.gitconfig" "${HOME}/.gitconfig"
+
+# neovim config
+mkdir -p "${HOME}/.config/nvim/" && \
+  cp -r nvim/. "${HOME}/.config/nvim/."
+
+# tmux config
 mkdir -p "${HOME}/.config/tmux/" && \
   cp -r tmux/. "${HOME}/.config/tmux/."
 
