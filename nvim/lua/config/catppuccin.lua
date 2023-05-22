0a2638c3065e055355f96f1299a27649895fffbf
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-05-22 22:53:45 +0800

    fix(nvim): enable catppuccin plugin integrations

--- nvim/lua/config/catppuccin.lua
+++ nvim/lua/config/catppuccin.lua
@@ -7,9 +7,36 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("catppuccin").setup {
-      integrations = { telescope = false },
-    }
+    require("catppuccin").setup({
+      flavor = "mocha",
+      term_colors = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        mason = true,
+        nvimtree = true,
+        treesitter = true,
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = false,
+        },
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            information = { "underline" },
+            warnings = { "underline" },
+          },
+          virtual_text = {
+            errors = { "bold", "italic" },
+            hints = { "italic" },
+            information = { "italic" },
+            warnings = { "italic" },
+          },
+        },
+      },
+    })
 
     vim.cmd.colorscheme("catppuccin")
     vim.keymap.set("n", "<leader>tt", function()
