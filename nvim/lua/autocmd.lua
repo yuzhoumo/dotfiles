2eab6d9f337d4554f1c854255f01f719f0dc91cc
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-06-03 00:32:57 +0800

    feat(nvim): delete trailing whitespace on save

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,11 +1,21 @@
--- highlight selection on yank
-local highlight_group =
-  vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_augroup("misc", { clear = true })
 
+-- highlight selection on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = "misc",
   pattern = "*",
+})
+
+-- strip trailing whitespace
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = "misc",
+  pattern = '*',
+  callback = function()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    vim.api.nvim_command("%s/\\s\\+$//e")
+    vim.api.nvim_win_set_cursor(0, cursor)
+  end,
 })
