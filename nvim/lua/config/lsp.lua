7a20b82d714bacc1ea2628bcfcf704c08bc91a99
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2025-05-05 13:19:19 -0700

    fix: remove deprecated vim.diagnostic.goto_

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -95,12 +95,13 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
       vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
       vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
       vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
       vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
       vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
       vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+
+      -- jump between diagnostics using [d and ]d
+      vim.diagnostic.config({ jump = { float = true }})
     end)
 
   end
