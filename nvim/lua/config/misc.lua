d69ba7298ae389578f368e0e72eeb10e4154417c
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-06-14 18:42:20 -0700

    feat: remove vim sleuth plugin

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -3,8 +3,4 @@ return {
     "tpope/vim-fugitive", -- git commands in nvim
     cmd = { "G", "Git" },
   },
-  {
-    "tpope/vim-sleuth",   -- automatic indent width adjustment
-    event = "BufReadPre",
-  },
 }
